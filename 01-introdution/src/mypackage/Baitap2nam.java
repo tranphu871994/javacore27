141dabb297c78c5417816e2a32da732b9c6cf136
tranphu871994 <tranphu871994@gmail.com>
2024-06-17 15:47:58 +0700

    commit

---
+++ 01-introdution/src/mypackage/Baitap2nam.java
@@ -0,0 +1,13 @@
+package mypackage;
+
+import java.lang.module.FindException;
+
+public class Baitap2nam {
+    public static void main(String[] args) {
+        String doiHoithoai = "If life gives you bad cards, be a good player.";
+
+        System.out.println(doiHoithoai.indexOf("f"));
+        System.out.println(doiHoithoai.lastIndexOf("f"));
+        System.out.println(doiHoithoai.replace("If","123"));
+    }
+}
